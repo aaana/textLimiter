30420e9de66f649627d25da457262d1b73f05ae9
Anna Huang <593880978@qq.com>
2018-09-29 23:04:06 +0800

    first commit

---
+++ textLimiter.js
@@ -0,0 +1,20 @@
+var flag = true;
+$('.textLimiter').on('compositionstart',function(){
+    console.log("composition--start");
+    flag = false;
+})
+$('.textLimiter').on('compositionend',function(){
+    console.log("composition--end"+$(this).val().length);
+    flag = true;
+
+})
+$('.textLimiter').on('input',function(){
+    console.log("input");
+    var _this = this;
+    setTimeout(function(){
+        if(flag){
+            $(_this).next().text("还可以输入" + ($(_this).attr('maxlength') - $(_this).val().length) + "个字");
+            console.log($(_this).val());
+        }
+    },0)
+})
